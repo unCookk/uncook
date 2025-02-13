de24628ad97a248cc417b86d74ecd289664b6c8d
jinjo Lim <dlawlswh123@gmail.com>
2025-02-13 19:16:46 +0900

    fix: state가 외부에 있는 코드 수정

--- app/api/resume/route.ts
+++ app/api/resume/route.ts
@@ -11,9 +11,9 @@ export async function GET() {
     return NextResponse.json({ success: true, data: resume }, { status: 200 })
   } catch (error) {
     console.error('resumes fetching 에러:', error)
-    return (
-      NextResponse.json({ success: false, message: '내부 서버 오류' }),
-      { status: 500 }
+    return NextResponse.json(
+      { success: false, message: '내부 서버 오류' },
+      { status: 500 },
     )
   }
 }
