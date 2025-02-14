9c1ff6c604d24350a1ebee961d625707d2384e6a
MR.J <cowhdals1111@gmail.com>
2025-02-14 22:08:40 +0900

    폴더명 케밥 케이스로 변경

--- app/_components/survey/survey-content.tsx
+++ app/_components/survey/survey-content.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { useState, useCallback } from 'react'
 
-import { QuestionBlock } from './contentElements/question-block'
+import { QuestionBlock } from './content-elements/question-block'
 import { surveyData } from './survey-data'
 
 import { addDocument } from '#/lib/firebase/firestore'
